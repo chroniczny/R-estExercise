404e436b4982cf5efd528d6fb96ee330b26cb68b
Jakub Kwiatkowski <jakub1kwiatkowski@gmail.com>
2016-08-20 11:54:19 +0200

    jq-workshop 6.infiniteSlider - małe poprawki, ciągle bez pkt7

--- 3. workshop/6.InfiniteSlider/js/app.js
+++ 3. workshop/6.InfiniteSlider/js/app.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
         console.log(lastImg.children(0).attr('src'));
 
 
-        var indxOfVisibleImg = 0;
+        var indxOfVisibleImg = 1; // to wżne żeby po władowaniu wczytywał pozycję nie 0 (bo to dubel obraka) a 1
         var imgWidth = 400; // szerokość obrazka
         var ulContener = $('.slider').find('ul');
 
@@ -26,12 +26,12 @@ $(document).ready(function () {
 
         var sliderWidth = $('li').length * imgWidth + "px"; // slider to pasek z obrazków 6 x szerokosc ale
         ulContener.outerWidth(sliderWidth); // biorę szerzej, zeby nie dodawać pikseli do 6xszerokosc obrazka
-        //var newPos = -400;
 
+// w css startujemy z pozycji ul left = -400px aby być na pierwotnie pierwszym obrazku (ale w DOM jest to element [1])
         btnNext.on('click', function (event) {
-            console.log('next_clicked');
-            var newPos = imgWidth * (1 + indxOfVisibleImg);
-            ulContener.animate({left: -newPos}, function () {
+            //console.log('next_clicked');
+            var newPos = -imgWidth * (1 + indxOfVisibleImg);
+            ulContener.animate({left: newPos}, function () {
                 if (indxOfVisibleImg < liEl.length - 1) {
                     indxOfVisibleImg = indxOfVisibleImg + 1; // zmienia się index obrazka widzialnego teraz
                 } else {
@@ -52,11 +52,8 @@ $(document).ready(function () {
                     ulContener.css({left: -newPos});
                     indxOfVisibleImg = liEl.length;
                 }
-
             });
-
         })
-
     }
 
     sliderA();
