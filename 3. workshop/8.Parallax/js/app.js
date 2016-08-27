2ad5b1efa868d1918d6532fa536bfd5d49fec433
Jakub Kwiatkowski <jakub1kwiatkowski@gmail.com>
2016-08-27 17:00:22 +0200

    parallax done ;)

--- 3. workshop/8.Parallax/js/app.js
+++ 3. workshop/8.Parallax/js/app.js
@@ -3,24 +3,26 @@
  */
 $(document).ready(function () {
     console.log('działa');
-// 2.
-    var scene = $('.scene');
-    var elem = scene.find('.element');
-// 4.
-    var oldMousePositionX = 0;
-    var oldMousePositionY = 0;
+
 
     function parallax() {
+        // 2.
+        var scene = $('.scene');
+        var elem = scene.find('.element');
+// 4.
+        var oldMousePositionX = 0;
+        var oldMousePositionY = 0;
+
 // 3.
         elem.each(function (idx, el) {
             //console.log($(el).attr('data-y'));
             var x = $(this).attr('data-x');
-            console.log(x, idx);
+            //console.log(x, idx);
             var y = $(this).attr('data-y');
-            console.log(y, idx);
+            //console.log(y, idx);
 
             var z = $(this).attr('data-z');
-            console.log(z, idx);
+            //console.log(z, idx);
 
             $(this).css('left', x + "px");
             $(this).css('top', y + "px");
@@ -30,28 +32,25 @@ $(document).ready(function () {
         scene.mouseenter(function (event) {
             var oldMousePositionX = event.offsetX;
             var oldMousePositionY = event.offsetY;
-            console.log("mouseENTER :" + oldMousePositionX, oldMousePositionY);
+            //console.log("mouseENTER :" + oldMousePositionX, oldMousePositionY);
         });
 
 
 // 4.
-        scene.on('mousemove', elem, function (event) {
+        scene.on('mousemove', function (event) {
             // 5.
             var currentMousePositionX = event.offsetX;
             var currentMousePositionY = event.offsetY;
-            console.log("mousemove :" + currentMousePositionX, currentMousePositionY);
+            //console.log("mousemove :" + currentMousePositionX, currentMousePositionY);
 
-            // 6.
-            $(event.target).on('mouseenter', function () {
-                //var changePosX = $(this).attr('data-x'); // option 1.
-                //var changePosY = $(this).attr('data-y');
-                var changePosX = $(this).width(); // option 2.
-                var changePosY = $(this).height();
+            // 6. uniknięcie migotania kwadratów
 
+            // podpowiedź od Kasi i Jakuba:
+            if (event.target!=this) { // jeśli wjezdzam na kwadrt to nie rób tego eventu...
+            console.log(event.target);
+                return;
+        }
 
-                currentMousePositionX = currentMousePositionX + changePosX;
-                currentMousePositionY = currentMousePositionY + changePosY;
-            });
 // 7.
             var mouseMoveX = currentMousePositionX - oldMousePositionX;
             var mouseMoveY = currentMousePositionY - oldMousePositionY;
@@ -60,24 +59,20 @@ $(document).ready(function () {
             // 8.
             elem.each(function (idx, el) {
                 var speed = $(this).attr('data-speed');
-                console.log(speed);
+                //console.log(speed);
                 var movLeftPos = $(this).offset().left + mouseMoveX * speed;
                 var movTopPos = $(this).offset().top + mouseMoveY * speed;
-                $(this).animate({
+                $(this).offset({
                     'left': movLeftPos,
                     'top': movTopPos
-                }); // zmieniłem na animację, zeby zaobserwować co się dzieje
-
-                // 9. gdzie???????????????????????
-
+                });
             });
+
+            // 9. gdzie? tu.
             oldMousePositionX = currentMousePositionX;
             oldMousePositionY = currentMousePositionY;
 
-
         });
-
-
     }
 
     parallax();
